75d728ed5e1622d60dc3ca22ff2989d3e9c05afc
Dave Carson <dave@onthefencedevelopment.com>
2020-04-14 18:51:15 +0100

    Added scanning & Validation of Contact QR Code

--- Fragments/ContactMaintenanceFragment.cs
+++ Fragments/ContactMaintenanceFragment.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using AndroidX.Fragment.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
 using Android.Views;
 using Android.Widget;
+using ZXing.Mobile;
+using ZXing;
 
 namespace ShhhSMS.Fragments
 {
@@ -20,8 +16,6 @@ namespace ShhhSMS.Fragments
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Create your fragment here
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -33,9 +27,49 @@ namespace ShhhSMS.Fragments
             return rootView;
         }
 
-        private void AddContact_Click(object sender, EventArgs e)
+        private async void AddContact_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            var scanner = new MobileBarcodeScanner();
+            scanner.TopText = "Scan Contact QR Code";
+            scanner.CancelButtonText = "Cancel";
+
+            var result = await scanner.Scan();
+
+            if (IsValidContactScan(result))
+            {
+                // TODO: If scan is valid, open 'Add New Contact' Fragment
+                var foo = 1;
+            }
+            else
+            {
+                Activity.RunOnUiThread(() => Toast.MakeText(Activity, "Invalid Scan - Please Try Again", ToastLength.Long).Show());
+            }
+        }
+
+        private bool IsValidContactScan(Result scannedData)
+        {
+            Guid contactId;
+
+            // First make sure that it was a QR Code that was scanned
+            if (scannedData.BarcodeFormat != BarcodeFormat.QR_CODE)
+                return false;
+            
+            var scanElements = scannedData.Text.Split("|");
+
+            // Ensure correct number of elements
+            if (scanElements.Length != 2)
+                return false;
+
+            // Ensure ContactId is a valid Guid
+            if (Guid.TryParse(scanElements[0], out contactId) == false)
+                return false;
+
+            // Ensure decoded PublicKey is a valid byte array of the correct length
+            var publicKeyBytes = Convert.FromBase64String(scanElements[1]);
+            if(publicKeyBytes.Length != 32)
+                return false;
+
+            return true;
         }
     }
 }
